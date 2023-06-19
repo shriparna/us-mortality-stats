f974bdc3a3b627f9188e80b9e13da2747135c2b6
VinnyShankar <vinayakshank@gmail.com>
2023-06-19 00:24:12 -0700

    horizontal bar chart

--- feat_vs/static/js/app.js
+++ feat_vs/static/js/app.js
@@ -1,54 +1,57 @@
-d3.json("/api/v1.0/bar_data")
+d3.json("/api/v1.0/states_list")
 .then(data =>
 {
-  console.log(data)
-  let causeArray = []
-  let deathArray = []
   for (x of data)
   {
-    causeArray.push(x["Cause Name"])
-    deathArray.push(x["Deaths"])
+    d3.select("#selDataset")
+    .append("option")
+    .attr("value",x)
+    .text(x)
   }
-  console.log(causeArray)
-  console.log(deathArray)
-  let trace1 =
-  {
-    x:deathArray,
-    y:causeArray,
-    type:"bar",
-    orientation:"h"
-  }
-
-  let bardata = [trace1]
-
-  Plotly.newPlot("bar",bardata)
-
 })
 
+function drawBars()
+{
+  d3.json("/api/v1.0/bar_data")
+  .then(data =>
+  {
+    console.log(data)
+    let causeArray = []
+    let deathArray = []
+    for (x of data)
+    {
+      causeArray.push(x["Cause Name"])
+      deathArray.push(x["Age-adjusted Death Rate"])
+    }
+    console.log(causeArray)
+    console.log(deathArray)
+    let trace1 =
+    {
+      x:deathArray,
+      y:causeArray,
+      type:"bar",
+      orientation:"h"
+    }
 
+    let barlayout =
+    {
+      title:`${data[0]["State"]} Age-adjusted Death Rate by Cause Name`,
+      yaxis:
+      {
+        automargin:true
+      }
+    }
 
+    let bardata = [trace1]
 
+    Plotly.newPlot("bar",bardata,barlayout)
 
+  })
+}
 
-// function barPlot(data)
-// {
-//   let barData =
-//   [
-//     {
-//       x:[],
-//       y:[],
-//       type:"bar"
-//     }
-//   ]
+drawBars()
 
-//   Plotly.newPlot("bar",barData)
-// }
-// let data = [
-//   {
-//     x: ["giraffes", "orangutans", "monkeys"],
-//     y: [20, 14, 23],
-//     type: "bar",
-//   },
-// ];
-
-// Plotly.newPlot("bar", data);
+function optionChanged()
+{
+  drawBars()
+}
