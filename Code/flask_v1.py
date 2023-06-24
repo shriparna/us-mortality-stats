102b7952d4586c482814761036e7da0daa017cb0
VinnyShankar <vinayakshank@gmail.com>
2023-06-24 15:37:29 -0700

    racing lines chart added

--- Code/flask_v1.py
+++ Code/flask_v1.py
@@ -31,6 +31,30 @@ def welcome():
 #################################################
 # USA JSON endpoint
 #################################################
+@app.route("/api/v1.0/racing")
+def racing_lines():
+    query = {"State":"United States",
+             "Cause Name":{"$not":{"$in":["All causes"]}}}
+    fields = {"_id":0,
+              "Age-adjusted Death Rate":1,
+              "Cause Name":1,
+              "Year":1}
+    result = get_from_mongo().find(query,fields).sort("Year",1)
+    keys = list(result[0].keys())
+    keys.reverse()
+    main_list = []
+    main_list.append(keys)
+    for x in result:
+        temp_list = []
+        temp_list.append(x["Age-adjusted Death Rate"])
+        temp_list.append(x["Cause Name"])
+        temp_list.append(x["Year"])
+        main_list.append(temp_list)
+    return dumps(main_list)
+
+#################################################
+# USA JSON endpoint
+#################################################
 @app.route("/api/v1.0/usJSON")
 def us_json():
     client = MongoClient(port=27017)
